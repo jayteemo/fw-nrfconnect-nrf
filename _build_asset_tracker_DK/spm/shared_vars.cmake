cf593afcb18afe7059c6097a1dd6a96a55811520
Justin Morton <justin.morton@nordicsemi.no>
2020-10-08 12:13:30 -0700

    ooops

--- _build_asset_tracker_DK/spm/shared_vars.cmake
+++
@@ -1,7 +0,0 @@
-set(spm_KERNEL_HEX_NAME zephyr.hex)
-set(spm_ZEPHYR_BINARY_DIR /Users/justin/Github/FORKS/fota_v2/nrf/_build_asset_tracker_DK/spm/zephyr)
-set(spm_KERNEL_ELF_NAME zephyr.elf)
-list(APPEND spm_BUILD_BYPRODUCTS /Users/justin/Github/FORKS/fota_v2/nrf/_build_asset_tracker_DK/spm/zephyr/zephyr.hex)
-list(APPEND spm_BUILD_BYPRODUCTS /Users/justin/Github/FORKS/fota_v2/nrf/_build_asset_tracker_DK/spm/zephyr/zephyr.elf)
-list(APPEND spm_BUILD_BYPRODUCTS
-    /Users/justin/Github/FORKS/fota_v2/nrf/_build_asset_tracker_DK/spm/libspmsecureentries.a)
