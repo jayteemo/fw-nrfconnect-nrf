548821648e4a5eb0658912b341951cd0a1055719
Justin Morton <justin.morton@nordicsemi.no>
2020-10-08 12:09:23 -0700

    settings config changes

---
+++ _build_asset_tracker_DK/mcuboot/shared_vars.cmake
@@ -0,0 +1,5 @@
+set(mcuboot_KERNEL_HEX_NAME zephyr.hex)
+set(mcuboot_ZEPHYR_BINARY_DIR /Users/justin/Github/FORKS/fota_v2/nrf/_build_asset_tracker_DK/mcuboot/zephyr)
+set(mcuboot_KERNEL_ELF_NAME zephyr.elf)
+list(APPEND mcuboot_BUILD_BYPRODUCTS /Users/justin/Github/FORKS/fota_v2/nrf/_build_asset_tracker_DK/mcuboot/zephyr/zephyr.hex)
+list(APPEND mcuboot_BUILD_BYPRODUCTS /Users/justin/Github/FORKS/fota_v2/nrf/_build_asset_tracker_DK/mcuboot/zephyr/zephyr.elf)
