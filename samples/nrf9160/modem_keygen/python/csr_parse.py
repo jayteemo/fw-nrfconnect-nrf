2f54827930a8f688d1ac2bd2803da27e002bde4e
Justin Morton <justin.morton@nordicsemi.no>
2021-04-07 16:01:25 -0700

    python script cleanup for new modem output

--- samples/nrf9160/modem_keygen/python/csr_parse.py
+++ samples/nrf9160/modem_keygen/python/csr_parse.py
@@ -26,7 +26,8 @@ device_type_dict = {
 
 def parse_args():
     parser = argparse.ArgumentParser(description="CSR parser")
-    parser.add_argument("-d", "--data", type=str, help="base64 string: CSR or attestation token", default="")
+    parser.add_argument("-k", "--keygen", type=str, help="base64url string: KEYGEN output", default="")
+    parser.add_argument("-a", "--attest", type=str, help="base64url string: ATTESTTOKEN output", default="")
     args = parser.parse_args()
     return args
 
@@ -38,56 +39,148 @@ def base64_decode(string):
     string = string + ("=" * padding)
     return base64.urlsafe_b64decode(string)
 
+def parse_cose(cose_str):
+    """
+    parse COSE payload.
+    """
+    if len(cose_str) == 0:
+        return
+
+    # Decode to binary and parse cbor
+    cose_bytes = base64_decode(cose_str)
+    cose_obj = loads(cose_bytes)
+
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+    print("COSE:")
+    # TODO: not sure what this first value is
+    print("  Value: " + cose_obj.value[0].hex())
+
+    # the unprotected header contains a map, where 4 indicates the key id
+    key_id = False
+    for key in cose_obj.value[1].keys():
+        if key == 4:
+            key_id = True
+            break
+
+    # Print key id value if found, otherwise just print the map
+    if key_id:
+        print("  Key ID: " + "0x" + cose_obj.value[1][4].hex())
+    else:
+        print("  " + str(cose_obj.value[1]))
+
+    # The COSE payload may contain a cbor attestation payload
+    # If present, decode the cbor and print
+    print("  ---------------")
+    print("  Attestation:")
+    if str(cose_obj.value[2]) != "None":
+        attest_obj = loads(cose_obj.value[2])
+        # TODO: not sure what the first value is
+        print("    ???:        " + str(attest_obj[0]))
+        print("    Dev. UUID:  " + attest_obj[1].hex())
+        print("    sec_tag:    " + "0x" + attest_obj[2].hex())
+        # TODO: Not sure what the 32 and 16 byte payloads are...
+        #       Hash/signatures of something
+        print("    32 bytes:   " + attest_obj[3].hex())
+        print("    16 bytes:   " + attest_obj[4].hex())
+    else:
+        print("    Not present")
+    print("  ---------------")
+
+    # Print the 64-bit signature
+    print("  Sig:")
+    print("      " + cose_obj.value[3].hex())
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+
+    return
+
+def parse_keygen_output(keygen_str):
+    """
+    parse keygen output.
+    """
+    print("\nParsing AT%KEYGEN output:\n")
+
+    # Input format: <base64url_body>.<base64url_cose>
+    #               cose portion is optional
+    body_cose = keygen_str.split('.')
+    body = body_cose[0]
+
+    # Decode base64url to binary
+    body_bytes = base64_decode(body)
+
+    # This can be either a CSR or device public key
+    try:
+        # Try to load CSR, if it fails, assume public key
+        csr_asn1 = OpenSSL.crypto.load_certificate_request(OpenSSL.crypto.FILETYPE_ASN1, body_bytes)
+
+    except OpenSSL.crypto.Error:
+        # Handle public key only
+        pub_key = OpenSSL.crypto.load_publickey(OpenSSL.crypto.FILETYPE_ASN1,body_bytes)
+        pub_key_str = OpenSSL.crypto.dump_publickey(FILETYPE_PEM, pub_key)
+
+    else:
+        # CSR loaded, print it
+        csr_pem_str = OpenSSL.crypto.dump_certificate_request(FILETYPE_PEM,csr_asn1)
+        csr_pem_list = str(csr_pem_str.decode()).split('\n')
+        for line in csr_pem_list:
+            print(line)
+
+        # Extract public key
+        pub_key_str = OpenSSL.crypto.dump_publickey(FILETYPE_PEM, csr_asn1.get_pubkey())
+
+    print("Device public key:")
+    print(pub_key_str.decode())
+
+    # Get optional cose
+    cose = ""
+    if len(body_cose) > 1:
+        cose = body_cose[1]
+
+    parse_cose(cose)
+
+    return
+
+def parse_attesttoken_output(atokout_str):
+    print("\nParsing AT%ATTESTTOKEN output:\n")
+
+    # Input format: <base64url_body>.<base64url_cose>
+    #               cose portion is optional
+    body_cose = atokout_str.split('.')
+    body = body_cose[0]
+
+    # Decode base64url to binary
+    body_bytes = base64_decode(body)
+    # Load into CBOR parser
+    body_obj = loads(body_bytes)
+
+    # Print parsed CBOR
+    print("---------------")
+    print("Msg Type:    " + msg_type_dict[body_obj[0]])
+    print("Dev UUID:    " + body_obj[1].hex())
+    print("Dev Type:    " + device_type_dict[body_obj[2]])
+    print("FW UUID:     " + body_obj[3].hex())
+    print("---------------")
+
+    # Get optional cose
+    cose = ""
+    if len(body_cose) > 1:
+        cose = body_cose[1]
+
+    parse_cose(cose)
+
+    return
+
 def main():
 
     if not len(sys.argv) > 1:
         raise RuntimeError('No input provided')
 
     args = parse_args()
-    if len(args.data) <= 0:
+    if len(args.keygen) > 0:
+        parse_keygen_output(args.keygen)
+    elif len(args.attest) > 0:
+        parse_attesttoken_output(args.attest)
+    else:
         raise RuntimeError("No input data provided")
-
-    body_cose = args.data.split('.')
-    body = body_cose[0]
-
-    cose = ""
-    if len(body_cose) > 1:
-        cose = body_cose[1]
-
-    body_bytes = base64_decode(body)
-
-    csr_asn1 = OpenSSL.crypto.load_certificate_request(OpenSSL.crypto.FILETYPE_ASN1, body_bytes)
-    csr_pem_str = OpenSSL.crypto.dump_certificate_request(FILETYPE_PEM,csr_asn1)
-
-    csr_pem_list = str(csr_pem_str.decode()).split('\n')
-    for line in csr_pem_list:
-        print(line)
-
-    print("Device public key:")
-    pub_key_str = OpenSSL.crypto.dump_publickey(FILETYPE_PEM, csr_asn1.get_pubkey())
-    print(pub_key_str.decode())
-
-    if len(cose):
-        cose_bytes = base64_decode(cose)
-        cose_obj = loads(cose_bytes)
-        print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
-        print("COSE:")
-        print("  " + cose_obj.value[0].hex())
-        print("  " + str(cose_obj.value[1]))
-
-        attest_obj = loads(cose_obj.value[2])
-        print("   ---------------")
-        print("   Attestation:")
-        print("               " + str(attest_obj[0]))
-        print("   Dev. UUID:  " + attest_obj[1].hex())
-        print("   Key ID:     " + attest_obj[2].hex())
-        print("   32 bytes:   " + attest_obj[3].hex())
-        print("   16 bytes:   " + attest_obj[4].hex())
-        print("   ---------------")
-        print("Sig:  ")
-        print("       " + cose_obj.value[3].hex())
-
-    print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
 
     return
 
--- samples/nrf9160/modem_keygen/python/readme.md
+++ samples/nrf9160/modem_keygen/python/readme.md
@@ -1,6 +1,21 @@
 CSR Parse Example
 
-Parse modem keygen output; with or without COSE portion.
+```
+usage: csr_parse.py [-h] [-k KEYGEN] [-a ATTEST]
 
-`python3 csr_parse.py -d <base64url keygen output>`
+CSR parser
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k KEYGEN, --keygen KEYGEN
+                        base64url string: KEYGEN output
+  -a ATTEST, --attest ATTEST
+                        base64url string: ATTESTTOKEN output
+```
+
+Parse modem keygen output; with or without COSE portion.
+`python3 csr_parse.py -k <base64url AT%KEYGEN output>`
+
+Parse modem attestation token output; with or without COSE portion.
+`python3 csr_parse.py -a <base64url AT%ATTESTTOKEN output>`
 
