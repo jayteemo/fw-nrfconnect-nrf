f1d638f1e8f36ed1b1ee30e43744552065f92a81
Justin Morton <justin.morton@nordicsemi.no>
2022-05-19 15:20:07 -0700

    net: lib: nrf_cloud: add full modem fota support
    Add support for full modem FOTA in the nrf_cloud library.
    CIA-383
    
    Signed-off-by: Justin Morton <justin.morton@nordicsemi.no>

--- include/net/nrf_cloud.h
+++ include/net/nrf_cloud.h
@@ -14,6 +14,7 @@
 #include <modem/modem_info.h>
 #endif
 #include <cJSON.h>
+#include <dfu/dfu_target_full_modem.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -219,10 +220,18 @@ enum nrf_cloud_fota_type {
 
 	/** Application update. */
 	NRF_CLOUD_FOTA_APPLICATION = NRF_CLOUD_FOTA_TYPE__FIRST,
-	/** Modem update. */
-	NRF_CLOUD_FOTA_MODEM = 1,
+	/** Delta modem update */
+	NRF_CLOUD_FOTA_MODEM_DELTA = 1,
 	/** Bootloader update. */
 	NRF_CLOUD_FOTA_BOOTLOADER = 2,
+
+	/* Types not handled by this library:
+	 * NRF_CLOUD_FOTA_BLE_BOOT = 3,
+	 * NRF_CLOUD_FOTA_BLE_SOFTDEVICE = 4,
+	 */
+
+	/** Full modem update */
+	NRF_CLOUD_FOTA_MODEM_FULL = 5,
 
 	NRF_CLOUD_FOTA_TYPE__INVALID
 };
@@ -339,8 +348,9 @@ struct nrf_cloud_svc_info_fota {
 	uint8_t bootloader:1;
 	uint8_t modem:1;
 	uint8_t application:1;
+	uint8_t modem_full:1;
 
-	uint8_t _rsvd:5;
+	uint8_t _rsvd:4;
 };
 
 /**@brief Controls which values are added to the UI array in the "serviceInfo" shadow section */
@@ -428,6 +438,10 @@ struct nrf_cloud_init_param {
 	 * is enabled; otherwise, NULL.
 	 */
 	char *client_id;
+	/** Flash device information required for full modem FOTA updates.
+	 * Only used if CONFIG_NRF_CLOUD_FOTA_FULL_MODEM_UPDATE is enabled.
+	*/
+	struct dfu_target_fmfu_fdev *fmfu_dev_inf;
 };
 
 /**
@@ -653,6 +667,7 @@ int nrf_cloud_jwt_generate(uint32_t time_valid_s, char * const jwt_buf, size_t j
  * @retval 0       A Pending FOTA job has been processed.
  * @retval -ENODEV No pending/unvalidated FOTA job exists.
  * @retval -ENOENT Error; unknown FOTA job type.
+ * @retval -ESRCH Error; not configured for FOTA job type.
  */
 int nrf_cloud_pending_fota_job_process(struct nrf_cloud_settings_fota_job * const job,
 				       bool * const reboot_required);
@@ -704,11 +719,38 @@ int nrf_cloud_handle_error_message(const char *const buf,
  *
  * @retval 0 Pending FOTA job processed.
  * @retval 1 Pending FOTA job processed and requires the application to perform a reboot or,
- *           for NRF_CLOUD_FOTA_MODEM types, reinitialization of the modem library.
+ *           for modem FOTA types, reinitialization of the modem library.
  * @retval -ENODEV No pending/unvalidated FOTA job exists.
+ * @retval -EIO Error; failed to load FOTA job info from settings module.
  * @retval -ENOENT Error; unknown FOTA job type.
  */
 int nrf_cloud_fota_pending_job_validate(enum nrf_cloud_fota_type * const fota_type_out);
+
+/**
+ * @brief Function to set the flash device used for full modem FOTA updates.
+ *
+ * @param[in] fmfu_dev_inf Flash device information.
+ *
+ * @retval 0 Flash device was successfully set.
+ * @return A negative value indicates an error.
+ */
+int nrf_cloud_fota_fmfu_dev_set(const struct dfu_target_fmfu_fdev *const fmfu_dev_inf);
+
+/**
+ * @brief Function to install a full modem update from flash. If successful,
+ *        reboot the device to complete the update.
+ *
+ * @retval 0 Modem update installed successfully.
+ * @return A negative value indicates an error. Modem update not installed.
+ */
+int nrf_cloud_fota_fmfu_apply(void);
+
+/**
+ * @brief Function to determine if FOTA type is modem related.
+ *
+ * @return true if FOTA is modem type, otherwise false.
+ */
+bool nrf_cloud_fota_is_type_modem(const enum nrf_cloud_fota_type type);
 
 /** @} */
 
