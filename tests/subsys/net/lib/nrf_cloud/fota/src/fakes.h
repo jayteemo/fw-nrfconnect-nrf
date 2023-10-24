766e3aa9106bad745faa8a82fddcec55fc4cc6bd
Justin Morton <justin.morton@nordicsemi.no>
2023-10-24 09:44:26 -0700

    WIP
    Signed-off-by: Justin Morton <justin.morton@nordicsemi.no>

---
+++ tests/subsys/net/lib/nrf_cloud/fota/src/fakes.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/fff.h>
+#include <zephyr/ztest.h>
+#include <net/nrf_cloud.h>
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VALUE_FUNC(int, nrf_cloud_fota_pending_job_validate, enum nrf_cloud_fota_type * const);
+FAKE_VALUE_FUNC(int, fota_download_init, fota_download_callback_t client_callback);
+
+int fake_nrf_cloud_fota_pending_job_validate__fails(enum nrf_cloud_fota_type * const type)
+{
+	ARG_UNUSED(type);
+	return -EIO;
+}
+
+int fake_nrf_cloud_fota_pending_job_validate__no_job(enum nrf_cloud_fota_type * const type)
+{
+	ARG_UNUSED(type);
+	return -ENODEV;
+}
+
+int fake_nrf_cloud_fota_pending_job_validate__succeeds_zero(enum nrf_cloud_fota_type * const type)
+{
+	ARG_UNUSED(type);
+	return 0;
+}
+
+int fake_nrf_cloud_fota_pending_job_validate__succeeds_one(enum nrf_cloud_fota_type * const type)
+{
+	ARG_UNUSED(type);
+	return 1;
+}
+
+int fota_download_init__fails(fota_download_callback_t client_callback)
+{
+	ARG_UNUSED(client_callback);
+	return -EIO;
+}
+
+int fota_download_init__succeeds(fota_download_callback_t client_callback)
+{
+	ARG_UNUSED(client_callback);
+	return 0;
+}
